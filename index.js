75ab3fa77bb826a706df369247575a9633c3f821
Lenin <reachlinlen@gmail.com>
2019-06-09 20:52:55 +0800

    PUT module working fine

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ var fs = require('fs')
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 //
-app.get('/tran', (req,res) => {
+app.get('/txn', (req,res) => {
   console.log('Express is working')
   res.send('Express is working')
 })
 //
-app.put('/tran', (req,res) => {
-  console.log('Received PUT request')
+app.put('/txn', jsonParser, (req,res) => {
+  console.log('Received PUT request: ')
+  console.log(req.body.body)
+  res.send("Transaction PUT Request Completed")
 })
 //
-app.listen(8000, () => {
-  console.log('Transaction Server listening on port 8000!')
+app.listen(8081, () => {
+  console.log('Transaction Server listening on port 8081!')
 })
 //
 app.use(function (req, res, next) {
   //Website allowed to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
   //Request methods allowed
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
@@ -29,7 +31,7 @@ app.use(function (req, res, next) {
 
   //Set to TRUE if you expect the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credential', true)
+  res.setHeader('Access-Control-Allow-Credentials', true)
 
   //Pass to next layer of middleware
   next()
